5bf332a3ac7e7a7bd7154501b73adda648173983
brandlew <hello@brandlew.co>
2025-05-12 14:34:20 -0700

    blog post titles

--- assets/js/contentonly.js
+++ assets/js/contentonly.js
@@ -13,7 +13,6 @@ function init() {
       draggable: ">1",
       accessibility: false,
       arrowShape: "m77.59 5.06-5.17-5.21-50 50 50 50 5.17-5.21-44.77-44.81z",
-      setGallerySize: false
     });
 }
 
